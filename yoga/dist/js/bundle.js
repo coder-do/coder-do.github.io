0c73774ae7c3e422462c25e325d0cc4e726a3db8
Meruzh Kiloyan <68124376+coder-do@users.noreply.github.com>
2020-08-23 13:04:23 +0400

    Update bundle.js

--- yoga/dist/js/bundle.js
+++ yoga/dist/js/bundle.js
@@ -354,7 +354,7 @@ module.exports = tabs;
 /***/ (function(module, exports) {
 
 function timer() {
-    let deadLine = '2020-08-29';
+    let deadLine = '2020-08-25';
 
     function getTime(end) {
         let t = Date.parse(end) - Date.parse(new Date()),
