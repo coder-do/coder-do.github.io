9dcdcc72165c0c19cd4c40ca4aa83a6a1b6a6cd8
Meruzh Kiloyan <68124376+coder-do@users.noreply.github.com>
2020-10-23 08:57:26 -0700

    Add files via upload

---
+++ build/precache-manifest.7531861c32117850fad08e19f19512be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed591fceb327a7ecb2ad010e45ef395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c8fb84a5f6375a47e27",
+    "url": "/static/css/main.9025fad9.chunk.css"
+  },
+  {
+    "revision": "7e89d83ca1cf14b8e9ef",
+    "url": "/static/js/2.7d6da17f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7d6da17f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8fb84a5f6375a47e27",
+    "url": "/static/js/main.b50da19e.chunk.js"
+  },
+  {
+    "revision": "08c49aac8c00567a29d6",
+    "url": "/static/js/runtime-main.59a75140.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
